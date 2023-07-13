fce6a06ba4d965ba3e05561f180eb619f57be815
Daniel Asay <danielasay@fas.harvard.edu>
2023-07-13 10:59:21 -0400

    modify name in config __init__, add prequal and qsiprep config files

--- dwiqc/config/__init__.py
+++ dwiqc/config/__init__.py
@@ -3,9 +3,23 @@ import yaml
 
 __dir__ = os.path.dirname(__file__)
 
-def default():
+def xnat_download():
     conf = os.path.join(
         __dir__,
         'dwiqc.yaml'
     )
     return conf
+
+def prequal_command():
+    cmd = os.path.join(
+        __dir__,
+        'prequal.yaml'
+    )
+    return cmd
+
+def qsiprep_command():
+    cmd = os.path.join(
+        __dir__,
+        'qsiprep.yaml'
+    )
+    return cmd
