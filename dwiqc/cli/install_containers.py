67d4ad840e9d52c24d44a738e27a03a2066c300c
Daniel Asay <danielasay@fas.harvard.edu>
2023-07-25 09:40:01 -0400

    adding download steps for all 3 containers

--- dwiqc/cli/install_containers.py
+++ dwiqc/cli/install_containers.py
@@ -8,7 +8,7 @@ import yaxil
 import glob
 import math
 import logging
-import subprocess as sp
+import subprocess
 import shutil
 from executors.models import Job, JobArray
 from bids import BIDSLayout
@@ -21,12 +21,34 @@ logger = logging.getLogger(__name__)
 
 chromium_link = 'https://www.dropbox.com/scl/fi/wgh2d6xtdgi87zaxc5m5l/chromium.sif?rlkey=go0y0yrgbdns6b2j8i55ufwro&dl=1'
 
-prequal_link = ''
+prequal_link = 'https://www.dropbox.com/scl/fi/p58pbj4v662ji1ax19lka/prequal_nrg.sif?rlkey=lmm9jspl81w6jjfvkmsmbd595&dl=1'
 
 qsiprep_link = 'https://www.dropbox.com/scl/fi/s8asxwmykyw7paqdcnaev/qsiprep.sif?rlkey=lnuffrzn9mf894q6j9tl2l2mc&dl=1'
 
+default_location = '~/.config/dwiqc/containers/'
 
 def do(args):
 
-	print(args.install_location)
+	os.makedirs(default_location, exist_ok=True)
+
+	logger.info('installing chromium...')
+
+	download_chromium = f'curl -L -s {chromium_link}'
+	proc1 = subprocess.Popen(download_chromium, shell=True, stdout=subprocess.PIPE)
+	proc1.communicate()
+
+	logger.info('installing prequal...')
+
+	download_prequal = f'curl -L -s {prequal_link}'
+	proc2 = subprocess.Popen(download_prequal, shell=True, stdout=subprocess.PIPE)
+	proc2.communicate()
+	
+	logger.info('installing qsiprep...')
+
+	download_qsiprep = f'curl -L -s {qsiprep_link}'
+	proc3 = subprocess.Popen(download_qsiprep, shell=True, stdout=subprocess.PIPE)
+	proc3.communicate()
+
+
+def symlink()
 	
