6cdddf99e3d589494535efb14b01bfcb99ec11dd
Daniel Asay <danielasay@fas.harvard.edu>
2023-08-07 14:50:38 -0400

    bug fix, wrong arg name

--- scripts/dwiQC.py
+++ scripts/dwiQC.py
@@ -103,7 +103,7 @@ def main():
         help='XNAT MR Session name')
     parser_tandem.add_argument('--project',
         help='XNAT Project name')
-    parser_get.add_argument('--xnat-config', default=config.xnat_download(),
+    parser_tandem.add_argument('--xnat-config', default=config.xnat_download(),
         help='dwiqc XNAT configuration file')
     parser_tandem.add_argument('--bids-dir', required=True,
         help='Output BIDS directory')
