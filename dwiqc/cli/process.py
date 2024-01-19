7c6307ed34d5faf3eaa2836827a20408e3dd371d
Daniel Asay <danielasay@fas.harvard.edu>
2024-01-19 11:48:40 -0500

    check where tempdir is

--- dwiqc/cli/process.py
+++ dwiqc/cli/process.py
@@ -110,6 +110,8 @@ def do(args):
             pipenv='/sw/apps/qsiprep'
         )
         os.environ['OPENBLAS_NUM_THREADS'] = '1'
+        print(qsiprep_taks)
+        sys.exit()
         logger.info(json.dumps(qsiprep_task.command, indent=1))
         #check_for_output(args, qsiprep_outdir)
         jarray.add(qsiprep_task.job)
