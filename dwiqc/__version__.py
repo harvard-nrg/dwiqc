fbd9b3c62704f49094c52a7fdd09d4ebbf037f50
Daniel Asay <danielasay@fas.harvard.edu>
2024-07-02 08:48:04 -0400

    fix the truncation so that all fmap files get truncated

--- dwiqc/__version__.py
+++ dwiqc/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'dwiqc'
 __description__ = 'Quality Assurance Pipeline for Diffusion MR Data'
 __url__ = 'https://github.com/harvard-nrg/dwiqc'
-__version__ = '1.6.6'
+__version__ = '1.6.7'
 __author__ = 'Neuroinformatics Research Group'
 __author_email__ = 'info@neuroinfo.org'
