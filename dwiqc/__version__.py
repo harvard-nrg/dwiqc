a5c03c3477cb48f4a0d52616106dc46efdba01e0
Daniel Asay <danielasay@fas.harvard.edu>
2025-01-29 11:48:21 -0500

    dwiqc version for new FSL version

--- dwiqc/__version__.py
+++ dwiqc/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'dwiqc'
 __description__ = 'Quality Assurance Pipeline for Diffusion MR Data'
 __url__ = 'https://github.com/harvard-nrg/dwiqc'
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 __author__ = 'Neuroinformatics Research Group'
 __author_email__ = 'info@neuroinfo.org'
